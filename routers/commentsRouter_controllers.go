6b991e9fd5b44f18ae99fa7fe546cc917b8c848a
shoshanav <shoshanav@3base.co.il>
2018-12-12 13:20:52 +0200

    Change to prod

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -6,21 +6,21 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"],
+	beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"],
 		beego.ControllerComments{
 			Method: "GetAll",
 			Router: `/`,
 			AllowHTTPMethods: []string{"get"},
 			Params: nil})
 
-	beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"],
+	beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"],
 		beego.ControllerComments{
 			Method: "Put",
 			Router: `/:restaurantId`,
 			AllowHTTPMethods: []string{"put"},
 			Params: nil})
 
-	beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/dragontail/controllers:RestaurantController"],
+	beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"] = append(beego.GlobalControllerRouter["github.com/shoshanav/dragontail-test/controllers:RestaurantController"],
 		beego.ControllerComments{
 			Method: "Delete",
 			Router: `/:restaurantId`,
